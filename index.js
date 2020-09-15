d6c105e07c91650e2ea6a74ef040d63af3171916
Daniel Vargas <davarg5@yahoo.com>
2020-09-15 13:54:34 -0400

    finished tasks 3-4

--- index.js
+++ index.js
@@ -48,25 +48,42 @@ function getFinals(data)
 };
 
 console.log(getFinals(fifaData));
+
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
+function getYears(callback) 
+{
+    const finals = callback(fifaData);
+    const years = finals.map((item) =>{
+        return item.Year;
+    });
+    return years;
 
 };
 
-getYears();
+console.log(getYears(getFinals));
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-
-    /* code here */
+function getWinners(callback) 
+{
+    const finals = callback(fifaData);
+    const winners = [];
+    finals.forEach((item) => {
+        if(item['Home Team Goals'] > item['Away Team Goals'])
+        {
+            winners.push(item['Home Team Name']);
+        }
+        else
+        {
+            winners.push(item['Away Team Name']);
+        }
+    });
+    return winners;
 
 };
 
-getWinners();
+console.log(getWinners(getFinals));
 
 /* Task 5: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
